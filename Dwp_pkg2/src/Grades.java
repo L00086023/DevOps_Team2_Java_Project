6137facab4f63bee13af975d0f4ee812869bde42
leafydog <l00092270@student.lyit.ie>
2017-07-04 09:28:17 +0100

    First commit of the day 04/07/2017

---
+++ Dwp_pkg2/src/Grades.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+
+/**
+ * @author Patrick Magee
+ *
+ */
+public class Grades {
+
+	/**
+	 * 
+	 */
+	private int studentid = 1234;
+	private String moduleid = "ENTPERF";
+	private double grade = 0.0;
+	
+	public int getStudentid() {
+		return studentid;
+	}
+	public void setStudentid(int studentid) {
+		this.studentid = studentid;
+	}
+	public String getModuleid() {
+		return moduleid;
+	}
+	public void setModuleid(String moduleid) {
+		this.moduleid = moduleid;
+	}
+	public double getGrade() {
+		return grade;
+	}
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+	public Grades() {
+		// TODO Auto-generated constructor stub
+		
+		
+		
+	}
+
+}
