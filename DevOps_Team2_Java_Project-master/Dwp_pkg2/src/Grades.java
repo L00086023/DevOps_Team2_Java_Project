19aa649f42c9b65b0379a1bc40b52ff13a07eda9
AnthonyQuinn <AnthonyQuinn@AnthonyQuinn-PC>
2017-08-15 19:45:12 +0100

    First commit . Changed the copyright details to DevOps Team Project (Pansies) @LYITLabs Letterkenny Co. Donegal

---
+++ DevOps_Team2_Java_Project-master/Dwp_pkg2/src/Grades.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+/**
+ * @author Patrick Magee
+ *
+ */
+public class Grades {
+
+	/**
+	 * 
+	 */
+	private int studentid = 123456;
+	private String moduleid = "ENTPERFGHY";
+	private double grade = 0.0;
+	
+	
+	public Grades() {
+		// TODO Auto-generated constructor stub
+		
+		
+		
+	}
+	public int getStudentid() {
+		return studentid;
+	}
+	public void setStudentid(int studentid) {
+		this.studentid = studentid;
+	}
+	public String getModuleid() {
+		return moduleid;
+	}
+	public void setModuleid(String moduleid) {
+		this.moduleid = moduleid;
+	}
+	public double getGrade() {
+		return grade;
+	}
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+
+}
