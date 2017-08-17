80a39d85951128cf48fc5993cc930c63f5eca94d
leafydog <l00092270@student.lyit.ie>
2017-08-17 12:16:43 +0100

    DB connector class and test class for connecting to was RDS

---
+++ DB class and test/Testing.java
@@ -0,0 +1,14 @@
+
+public class Testing {
+
+	public Testing() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		DBManager.dbConnection();
+		
+
+	}
+
+}
