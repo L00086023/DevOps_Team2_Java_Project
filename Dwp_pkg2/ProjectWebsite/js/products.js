d430fee384c2bb26e07188e90c88b309a54d6caa
Owen Lyons <l00137564@student.lyit.ie>
2017-09-05 12:16:01 +0100

    Deleted irrelevant JS again, added quantity and colours to product pages

---
+++ Dwp_pkg2/ProjectWebsite/js/products.js
@@ -0,0 +1,7 @@
+/*$('.field-product--option').hide();*/
+
+/*$('.field-product--option option').each(function() {
+    $('.options').append("<button>" + $(this).val() + "</button>")
+});*/
+$('.field-product--option').attr('size','3');
+  //$('.field-product--option').element.setAttribute("size", 3);
