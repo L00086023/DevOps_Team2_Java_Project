f3e52b0c8506753aaf34554a62d2bb1ad16294fa
L00137564 <l00137564@student.lyit.ie>
2017-09-06 12:47:01 +0100

    Fixed code that was purposefully broken for JaCoCo test

--- DB class and test/DBManager.java
+++ DB class and test/DBManager.java
@@ -1,13 +1,13 @@
 
-///import java.sql.Connection;
-//import java.sql.Date;
-//import java.sql.DriverManager;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.text.DateFormat;
-//import java.text.SimpleDateFormat;
-//import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
@@ -15,18 +15,18 @@ import javax.swing.JTextField;
 import com.mysql.jdbc.PreparedStatement;
 public class DBManager {
 	private static String url = "jdbc:mysql://l00092270.cirzp9u9acjn.eu-west-1.rds.amazonaws.com:3306/";
-	//private static String dbName = "DevOpsPansies?useSSL=false";
-	//private static String driver = "com.mysql.jdbc.Driver";
-	//private static String userName = "awsstudent";
-	//private static String password = "password";  
-	//private static Statement statement = null;
+	private static String dbName = "DevOpsPansies?useSSL=false";
+	private static String driver = "com.mysql.jdbc.Driver";
+	private static String userName = "awsstudent";
+	private static String password = "password";  
+	private static Statement statement = null;
 	private static ResultSet rs = null;
 	private static PreparedStatement sp1=null;
 
-	//public DBManager() {
+	public DBManager() {
 		
 
-	//}
+	}
 	public static void dbConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -41,8 +41,8 @@ public class DBManager {
 			
 			}
 	}
-	//public static String getAdminName(String name) {
-		//try{
+	public static String getAdminName(String name) {
+		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection conn = DriverManager.getConnection(url+dbName,userName,password);
 			statement=conn.createStatement();
@@ -52,7 +52,7 @@ public class DBManager {
 			  
 			conn.close();
 		} 
-		//catch (Exception e) {
+		catch (Exception e) {
 			e.printStackTrace();
 		}  
 		return name;
