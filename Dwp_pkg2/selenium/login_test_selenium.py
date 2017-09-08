6232d66a92e259f7dd23309194fd19e8de263c3a
L00137564 <l00137564@student.lyit.ie>
2017-09-08 19:02:41 +0100

    created selenium login test

---
+++ Dwp_pkg2/selenium/login_test_selenium.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import time as t
+
+driver = webdriver.Firefox(executable_path=r'C:\SeleniumGecko\geckodriver.exe')
+driver.get("http://localhost/DevOps_Team2_Java_Project-master/Dwp_pkg2/ProjectWebsite/index.php")
+
+login = driver.find_element_by_xpath("//*[contains(text(), 'Login')]")
+login.click()
+
+t.sleep(5)
+
+email = driver.find_element_by_id("email")
+email.send_keys("gimp@gmail.com")
+
+password = driver.find_element_by_id("password")
+password.send_keys("dorrian")
+
+submit = driver.find_element_by_id("submit")
+submit.click()
+
+t.sleep(10)
+
+assert "No results found." not in driver.page_source
+
+driver.close()
