4311e5eed16c6ece3378cffcde681a2ada5e579d
L00137564 <l00137564@student.lyit.ie>
2017-09-08 19:26:07 +0100

    integrated cart into existing website template, still work to do

--- Dwp_pkg2/ProjectWebsite/cartindex.php
+++ Dwp_pkg2/ProjectWebsite/cartindex.php
@@ -1,5 +1,19 @@
 <?php
 session_start();
+?>
+
+<?php
+						if(!(isset($_SESSION['Email']))){
+						$includeMode = "LoggedOut";
+						$cssFileName = 'notLoggedIn.css';
+						}else{
+						$includeMode = "LoggedIn";
+						$cssFileName = 'styles2.0.css';
+						}
+	?>
+
+	<?php
+session_start();
 require_once("dbcontroller.php");
 $db_handle = new DBController();
 if(!empty($_GET["action"])) {
@@ -45,34 +59,139 @@ switch($_GET["action"]) {
 
 ?>
 
-<?php
-						if(!(isset($_SESSION['Email']))){
-						$includeMode = "LoggedOut";
-						$cssFileName = 'notLoggedIn.css';
-						}else{
-						$includeMode = "LoggedIn";
-						$cssFileName = 'styles2.0.css';
-						}
-	?>
 
-?>
-<HTML>
-<HEAD>
-<TITLE>Shopping Cart <?php echo $includeMode; ?></TITLE>
-<link href="<?php echo $cssFileName;?>" type="text/css" rel="stylesheet" />
-</HEAD>
-<BODY>
+
+<!-- ################################################################################ --><!-- ################################################################################ -->
+<!-- ################################################################################ --><!-- ################################################################################ -->
+<!-- ################################################################################ --><!-- ################################################################################ -->
+<!doctype html>
+
+<html><!-- InstanceBegin template="/Templates/MainTemp.dwt" codeOutsideHTMLIsLocked="false" -->
+
+	<head>
+		<!-- ################################################################################ -->
+			<title>Shopping Cart <?php echo $includeMode; ?></title>
+
+
+						<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+										<!-- jQuery library -->
+										<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+										<!-- Latest compiled JavaScript -->
+										<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+						<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,400italic,300italic' rel='stylesheet' type='text/css'>
+						<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
+
+					
+						
+						<link href="css/<?php echo $cssFileName;?>" rel="stylesheet" type="text/css" media="screen">
+						
+					<link href="cart.css" rel="stylesheet" type="text/css" media="screen">
+					<link href="css/styles.css" rel="stylesheet" type="text/css" media="screen">
+
+					<link href="css/background_images.css" rel="stylesheet" type="text/css" media="screen">
+						
+
+
+
+	</head>
+		
+	<!-- ################################################################################ -->
+	<!-- ################################################################################ -->
+	<!-- ################################################################################ -->
+    <body>
+		<div id="wrapper">
+				<!-- ################################################################################ -->
+					<div id="top">
+						<div id="logo">
+								<a href="index.html"> <img src="images/logo.jpg"> </a>
+						</div><!--Close logo div-->
+
+						<div id="login">
+										
+              <?Php
+							if(!(isset($_SESSION['Email']))){
+							echo "<div id=loginlink> <a href=http://localhost/DevOps_Team2_Java_Project-master/Dwp_pkg2/ProjectWebsite/login.php><span>Login</span></a> </div>";
+							echo "<div id=reglink> <a href=http://localhost/DevOps_Team2_Java_Project-master/Dwp_pkg2/ProjectWebsite/register.php><span>register</span></a> </div>";
+							
+							}else{
+							echo "
+							 <div id=loginlink> <a href=http://localhost/DevOps_Team2_Java_Project-master/Dwp_pkg2/ProjectWebsite/logout.php>logout</a></div><div id=reglink><a href=http://localhost/DevOps_Team2_Java_Project-master/Dwp_pkg2/ProjectWebsite/Account.php> \t\t\tDetails  <br></a></div> <br>Welcome $_SESSION[Email]";
+
+							
+							}
+							?>
+										  <?php 
+
+							if(isset($_GET['logout']))
+							{
+							  session_unset();
+							  session_destroy();
+							  echo "you have been logged out";
+
+
+							}
+							?>
+						</div><!--Close login div-->
+						
+						<div id="phone_addr">
+							<ul>
+								 (074) - 9123456 <br>
+								 Main St. Letterkenny, <br>Co.Donegal, Ireland 
+							</ul>
+						</div><!--Close phone_addr div-->
+						
+				</div><!--Close top div-->
+				<!-- ################################################################################ -->
+				<div id="topnav">
+						<ul>
+						<?Php
+							if(!(isset($_SESSION['Email']))){
+
+								echo "<li class=active><a href=http://localhost/DevOps_Team2_Java_Project-master/Dwp_pkg2/ProjectWebsite/index.php><span>Home</span></a></li>
+								<li class=active><a href='products.php'><span>Products</span></a></li>
+								<li class=active><a href='contact.php'><span>Contact Us</span></a></li>
+								<li class=has-sub><a href='about.php'><span>About Us</span></a></li>";
+							
+							
+							}else{
+							echo "<li class=active><a href=http://localhost/DevOps_Team2_Java_Project-master/Dwp_pkg2/ProjectWebsite/index.php><span>Home</span></a></li>
+								<li class=active><a href='products.php'><span>Products</span></a></li>
+								<li class=active><a href='contact.php'><span>Contact Us</span></a></li>
+								<li class=has-sub><a href='about.php'><span>About Us</span></a></li>
+								<li class=has-sub><a href='about.php'><span>Specials</span></a></li>";
+							
+							}
+						?>
+				</div>
+				<!-- ################################################################################ -->
+            
+				
+					<div id="product-content"> 
+
+		<!-- #################################################################################################################################### -->
+		<!-- #################################################################################################################################### -->
+		<!-- #################################################################################################################################### -->
+
+
+
+
 <div id="shopping-cart">
 <div class="txt-heading">Shopping Cart <a id="btnEmpty" href="cartindex.php?action=empty">Empty Cart</a></div>
 <?php
 if(isset($_SESSION["cart_item"])){
     $item_total = 0;
 ?>	
-<table cellpadding="10" cellspacing="1">
+
+
+
+<table cellpadding="20" cellspacing="1">
 <tbody>
 <tr>
-<th style="text-align:left;"><strong>Name</strong></th>
-<th style="text-align:left;"><strong>Code</strong></th>
+<div id="name"><th style="text-align:left;"><strong>Name</strong></th></div>
+<div id="code"><th style="text-align:left;"><strong>Code</strong></th>
 <th style="text-align:left;"><strong>Colour</strong></th>
 <th style="text-align:right;"><strong>Quantity</strong></th>
 <th style="text-align:right;"><strong>Price</strong></th>
@@ -98,7 +217,10 @@ if(isset($_SESSION["cart_item"])){
 <td colspan="5" align=right><strong>Total:</strong> <?php echo "&euro;".$item_total; ?></td>
 </tr>
 </tbody>
-</table>		
+</table>
+
+
+
   <?php
 }
 ?>
@@ -125,5 +247,30 @@ if(isset($_SESSION["cart_item"])){
 	}
 	?>
 </div>
-</BODY>
-</HTML>
+
+
+		<!-- #################################################################################################################################### -->
+		<!-- #################################################################################################################################### -->
+		<!-- #################################################################################################################################### -->
+					
+						</div> <!-- end product-content div -->
+	
+            <!-- ################################################################################ -->
+			
+       <div id="footer">
+									<p>&copy;Copyright 2017 &bull; All Rights Reserved &bull; Dev Ops Team 2 Project(Pansies) @ LYITLabs  &bull; LYIT Letterkenny Co.Donegal</p>
+									<div id="social-media">
+								<ul>
+										<li><a href="http://www.facebook.com"><img src="images/icons/facebook.png"></a></li>
+										<li><a href="http://www.twitter.com"><img src="images/icons/twitter.png"></a></li>
+										<li><a href="http://www.instagram.com"><img src="images/icons/Instagram-Icon.png"></a></li>
+										<li><a href="http://www.youtube.com"><img src="images/icons/youtube.png"></a></li>
+								</ul>
+						</div><!--Close social-media div-->
+									
+						</div><!--Close
+		<!-- ################################################################################ -->
+		</div>
+        </div>	
+    </body>
+</html>
